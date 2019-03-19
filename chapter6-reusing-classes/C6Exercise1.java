90dbcab5ca0dd7a5ccf130ab57201a2fa04461ac
josephyoung <2t@live.com>
2019-03-19 16:23:20 +0800

    chapter 6 exercise 1

---
+++ chapter6-reusing-classes/C6Exercise1.java
@@ -0,0 +1,24 @@
+/**
+ * C6Exercise1:
+ * (2)Create a simple class. Inside a second class, define a reference to an
+ * object of the first class. Use lazy initialization to instantiate this object.
+ */
+class Simple {}
+
+class Second {
+  Simple simple;
+
+  String getSimple() {
+    simple = new Simple();
+    return "lazy initialization a simple class: " + simple;
+  }
+}
+
+public class C6Exercise1 {
+
+  public static void main(String[] args) {
+    Second foo = new Second();
+    System.out.println(foo.simple);
+    System.out.println(foo.getSimple());
+  }
+}
