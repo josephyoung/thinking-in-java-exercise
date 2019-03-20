43f165d9dc7092024bbb1b7dd20d278bfb25cd9f
josephyoung <2t@live.com>
2019-03-20 17:06:33 +0800

    chapter 6 exercise 24

--- chapter6-reusing-classes/C6Exercise24.java
+++ chapter6-reusing-classes/C6Exercise24.java
@@ -1,8 +1,42 @@
 /**
  * C6Exercise24:
- * (2)
+ * (2)In Beetle.java, inherit a specific type of beetle from class Beetle,
+ * following the same format as the existing classes. Trace and explain the ouput.
  */
-public class C6Exercise24 {
 
-  
+class Insect {
+  private int i = 9;
+  protected int j;
+  Insect() {
+    System.out.println("i = " + i + ", j = " + j);
+    j = 39;
+  }
+  private static int x1 = printInit("static Insect.x1 initialized");
+  static int printInit(String s) {
+    System.out.println(s);
+    return 47;
+  }
+}
+
+class Beetle extends Insect {
+  private int k = printInit("Beetel.k initialized");
+  Beetle() {
+    System.out.println("k = " + k);
+    System.out.println("j = " + j);
+  }
+  private static int x2 = printInit("static Beetle.x2 initialized");
+}
+
+class C6Exercise24 extends Beetle {
+  private int l = printInit("C6Exercise24.l initialized");
+  C6Exercise24() {
+    System.out.println("l = " + l);
+    System.out.println("j = " + j);
+  }
+  private static int x3 = printInit("static C6Exercise24.x3 initialized");
+
+  public static void main(String[] args) {
+    System.out.println("C6Exercise24 constructor");
+    C6Exercise24 foo = new C6Exercise24();
+  }
 }
