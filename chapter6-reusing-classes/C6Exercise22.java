0fe822af84c364a9a064cf0bd926aa0119d93eb3
josephyoung <2t@live.com>
2019-03-20 16:11:58 +0800

    chapter 6 exercise 22

---
+++ chapter6-reusing-classes/C6Exercise22.java
@@ -0,0 +1,13 @@
+/**
+ * C6Exercise22:
+ * (1)Create a final Class and attempt to inherit from it.
+ */
+
+final class FinalClass {}
+
+public class C6Exercise22 extends FinalClass{
+
+  public static void main(String[] args) {
+    
+  }
+}
